523f69691e1fecbcc06c18afa618f9fdc30954f3
vivekkuyadav21@gmail.com <vivekkuyadav21@gmail.com>
2026-03-25 02:27:24 +0530

    testing the github workflow - 2

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 print("Github actions is in workflow!!....")
 print("Guillotine learning github action for Continuous integration")
+print("learning containerization along")
 ls = [1,2,3,4,5,6,7,8,9,10]
 arr = np.array(ls)
 s = pd.Series(arr)
