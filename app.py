817ac8976e284efd7e7cd32978b4248c0945f915
vivekkuyadav21@gmail.com <vivekkuyadav21@gmail.com>
2026-03-28 18:45:20 +0530

    changed the repo name into lowercase as it was hindering for build and push workflow

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 print("Github actions is in workflow!!....")
 print("Guillotine learning github action for Continuous integration")
 print("learning containerization along with github Actions")
+print("learning to build and push into container through hiding username and passkeys into the security and variables.")
 ls = [1,2,3,4,5,6,7,8,9,10]
 arr = np.array(ls)
 s = pd.Series(arr)
